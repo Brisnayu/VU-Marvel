1cbd7546f00467ddfa637da0bf3a46860d47699d
BrisnaPaez <brisnapaez.13@gmail.com>
2024-07-07 20:28:51 +0200

    Create component principal-card

--- src/app/features/pages/components/series/series.component.ts
+++ src/app/features/pages/components/series/series.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SeriesServices } from '../../../../core/services/seriesServices/series-services.service';
 import { Series } from '../../../../core/models/series.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-series',
@@ -9,18 +10,24 @@ import { Series } from '../../../../core/models/series.model';
 })
 export class SeriesComponent implements OnInit {
   private seriesService = inject(SeriesServices);
+  private router = inject(Router);
   listSeries: Series[] = [];
 
   ngOnInit(): void {
-
     this.seriesService.getSeries().subscribe(
       (series) => {
         this.listSeries = series;
-        console.log(this.listSeries);
+        // console.log(this.listSeries);
       },
       (error) => {
         console.error('Error al obtener las series:', error);
       }
     );
+  }
+
+  goToListComics(id: number) {
+
+      this.router.navigate([`/series/${id}/comics`])
+    
   }
 }
